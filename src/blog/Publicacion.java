33765e0f83199073a6890706cadd261145623f9a
Diego <pito.rodriguez05@gmail.com>
2026-04-23 13:54:39 -0600

    agregar atributos publicacion

--- src/blog/Publicacion.java
+++ src/blog/Publicacion.java
@@ -1,5 +1,14 @@
 package blog;
 
-public class Publicacion {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-}
+public class Publicacion {
+    private static int contadorCodigo = 1;
+
+    private int codigo;
+    private String titulo;
+    private String texto;
+    private String nombreCreador;
+    private LocalDateTime fechaPublicacion;
+    private ArrayList<Comentario> comentarios;

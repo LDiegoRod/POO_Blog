435b7e63fdf72cb25d7fa680e60e415f87d86346
Diego <pito.rodriguez05@gmail.com>
2026-04-23 14:06:32 -0600

    metodos de publicacion

--- src/blog/Publicacion.java
+++ src/blog/Publicacion.java
@@ -12,3 +12,44 @@ public class Publicacion {
     private String nombreCreador;
     private LocalDateTime fechaPublicacion;
     private ArrayList<Comentario> comentarios;
+
+    public Publicacion(String titulo, String texto, String nombreCreador) {
+        this.codigo = contadorCodigo;
+        contadorCodigo++;
+        this.titulo = titulo;
+        this.texto = texto;
+        this.nombreCreador = nombreCreador;
+        this.fechaPublicacion = LocalDateTime.now();
+        this.comentarios = new ArrayList<>();
+    }
+
+    public void agregarComentario(String emailAutor, String direccionIp, String texto) {
+        Comentario nuevoComentario = new Comentario(emailAutor, direccionIp, texto);
+        comentarios.add(nuevoComentario);
+    }
+
+    public void borrarComentario(int posicion) {
+        if (posicion >= 0 && posicion < comentarios.size()) {
+            comentarios.remove(posicion);
+        } else {
+            System.out.println("Posicion de comentario no valida.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String resultado = "Publicacion\n";
+        resultado += "Codigo: " + codigo + "\n";
+        resultado += "Titulo: " + titulo + "\n";
+        resultado += "Texto: " + texto + "\n";
+        resultado += "Creador: " + nombreCreador + "\n";
+        resultado += "Fecha de publicacion: " + fechaPublicacion + "\n";
+        resultado += "Comentarios:\n";
+
+        for (int i = 0; i < comentarios.size(); i++) {
+            resultado += "[" + i + "] " + comentarios.get(i).toString() + "\n";
+        }
+
+        return resultado;
+    }
+}

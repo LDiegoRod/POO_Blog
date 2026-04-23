0da1d112908019367ff7276535dbc7349cf73677
Diego <pito.rodriguez05@gmail.com>
2026-04-23 14:25:58 -0600

    atributos blog

--- src/blog/Blog.java
+++ src/blog/Blog.java
@@ -1,5 +1,15 @@
 package blog;
 
-public class Blog {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-}
+public class Blog {
+    private static int contadorCodigos = 1;
+
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private LocalDateTime fechaCreacion;
+    private ArrayList<Publicacion> publicaciones;

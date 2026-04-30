86dddd33e0b8704bb6e57e617c1311f87686cfad
Diego <pito.rodriguez05@gmail.com>
2026-04-29 18:48:58 -0600

    codigo de controladora

--- src/control/Controladora.java
+++ src/control/Controladora.java
@@ -9,5 +9,28 @@ public class Controladora {
     private Map<Integer, Blog> blogs;
 
     public Controladora() {
+        blogs = new TreeMap<>();
+        cargarDatosIniciales();
+    }
 
+    public void crearBlog(String nombre, String descripcion) {
+        Blog b = new Blog(nombre, descripcion);
+        blogs.put(b.getCodigo(), b);
+    }
+
+    public void borrarBlog(int codigoBlog) {
+        if (!blogs.containsKey(codigoBlog)) {
+            throw new IllegalArgumentException("No existe un blog con ese codigo.");
+        }
+        blogs.remove(codigoBlog);
+    }
+
+    public Map<Integer, String> obtenerBlogs() {
+        Map<Integer, String> resultado = new TreeMap<>();
+
+        for (Blog b : blogs.values()) {
+            resultado.put(b.getCodigo(), b.getNombre());
+        }
+
+        return resultado;
     }

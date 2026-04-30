238079d4b29683bd45355fa3a33d12f7065099cf
Diego <pito.rodriguez05@gmail.com>
2026-04-29 19:11:57 -0600

    carga de datos

--- src/control/Controladora.java
+++ src/control/Controladora.java
@@ -74,3 +74,36 @@ public class Controladora {
         blogs.get(codigoBlog).borrarComentario(codigoPublicacion, posicion);
     }
 
+    private void cargarDatosIniciales() {
+        crearBlog("Cartas Yu-Gi-Oh", "Blog dedicado a estrategias, cartas y curiosidades de Yu-Gi-Oh");
+        crearBlog("Futbol y censura", "Blog sobre casos curiosos donde el futbol ha sido restringido o prohibido en ciertos paises");
+
+        crearPublicacion(1, "Las mejores cartas trampa para defenderte", 
+                "En este post se comentan algunas cartas trampa utiles para frenar ataques y controlar el duelo.", 
+                "Luis");
+
+        crearPublicacion(1, "Por que el Mago Oscuro sigue siendo tan famoso", 
+                "Aunque han salido cartas mas modernas, el Mago Oscuro sigue siendo una de las cartas mas representativas de Yu-Gi-Oh.", 
+                "Diego");
+
+        crearPublicacion(2, "Paises donde el futbol ha sido restringido temporalmente", 
+                "En algunos lugares se han aplicado restricciones al futbol por razones politicas, sociales o de seguridad.", 
+                "Luis");
+
+        crearPublicacion(2, "Razones por las que un torneo puede ser prohibido", 
+                "Un torneo puede ser suspendido o prohibido por conflictos, falta de permisos o situaciones extraordinarias.", 
+                "Diego");
+
+        agregarComentario(1, 1, "luis@mail.com", "192.168.0.10", "Buen tema, casi nunca se habla de las cartas trampa.");
+        agregarComentario(1, 1, "diego@mail.com", "192.168.0.11", "Seria bueno mencionar tambien cartas de contraefecto.");
+
+        agregarComentario(1, 2, "grace@mail.com", "192.168.0.12", "El Mago Oscuro es iconico, aunque ya no sea tan competitivo.");
+        agregarComentario(1, 2, "dorys@mail.com", "192.168.0.13", "Me gusto el enfoque nostalgico de la publicacion.");
+
+        agregarComentario(2, 3, "rulio@mail.com", "192.168.0.14", "Interesante, no sabia que eso habia pasado en algunos paises.");
+        agregarComentario(2, 3, "dima@mail.com", "192.168.0.15", "Podrias agregar ejemplos concretos de casos reales.");
+
+        agregarComentario(2, 4, "lidiete@mail.com", "192.168.0.16", "Buen resumen, se entiende facil.");
+        agregarComentario(2, 4, "mickey@mail.com", "192.168.0.17", "Seria interesante compararlo con otros deportes.");
+    }
+}

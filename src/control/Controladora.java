2222252a6a4dd6aeb2f1ee83514c00e2eaa2b339
Diego <pito.rodriguez05@gmail.com>
2026-04-29 18:43:18 -0600

    creacion de paquete y clase control

---
+++ src/control/Controladora.java
@@ -0,0 +1,13 @@
+package control;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import blog.Blog;
+
+public class Controladora {
+    private Map<Integer, Blog> blogs;
+
+    public Controladora() {
+
+    }
